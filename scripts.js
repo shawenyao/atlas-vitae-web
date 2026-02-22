6a68fbf2856065071ecb6df5974177a806d19216
Wenyao <38875149+shawenyao@users.noreply.github.com>
2026-02-22 01:54:45 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -122,7 +122,7 @@ viewport.addEventListener('mouseup', e => handleEnd(e, e.clientX));
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      random_page_flip_count = Math.floor(Math.random() * 4) + 1;
+      random_page_flip_count = Math.floor(Math.random() * 5) + 1;
       for (let i = 0; i < random_page_flip_count; i++) {
         setTimeout(() => {
           goNext();
